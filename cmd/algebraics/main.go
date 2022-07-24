ceae338ce862036ab490659847b06047ca39fc8b
vsaraph <vikram.saraph.22@gmail.com>
2022-07-24 12:55:39 -0400

    findroots

--- cmd/algebraics/main.go
+++ cmd/algebraics/main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
-	"go/build"
-	"image"
-	"image/draw"
+	"fmt"
 	_ "image/png"
-	"log"
 	"math"
 	"math/cmplx"
 	"math/rand"
-	"os"
-	"runtime"
-
-	"github.com/go-gl/gl/v2.1/gl"
-	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
 // https://www.wolfe.id.au/2020/03/10/starting-a-go-project/
 // https://commons.wikimedia.org/wiki/File:Algebraicszoom.png
 // https://kylewbanks.com/blog/tutorial-opengl-with-golang-part-1-hello-opengl
-
-// VAO and VBO tutorial: https://www.youtube.com/watch?v=WMiggUPst-Q
-// https://stackoverflow.com/a/50408198
-// glVertex2f deprecated... explains why there is no gl.Vertex2f
-
-// newer versions do have compatibility mode that allows one to use older API
-// older versions are also available, switching to 2.1 since it works. That's good enough
-
-// spinning cube example here:
-// https://github.com/go-gl/example/tree/master/gl21-cube
+// https://learnopengl.com/book/book_pdf.pdf
 
 func rand_double(max float64) float64 {
 	return rand.Float64() * max
@@ -36,7 +19,7 @@ func rand_double(max float64) float64 {
 
 func findroots_inner(coefs []complex128, deg int, roots []complex128) []complex128 {
 	var root complex128
-	if (deg == 1) {
+	if deg == 1 {
 		root = -coefs[0] / coefs[1]
 		roots = append(roots, root)
 		return roots
@@ -44,20 +27,20 @@ func findroots_inner(coefs []complex128, deg int, roots []complex128) []complex1
 
 	var n int
 	var f, d, p, or complex128
-	root = complex(rand_double(2) - 1, rand_double(2) - 1)
+	root = complex(rand_double(2)-1, rand_double(2)-1)
 	var i int = 0
 	var j int = 0
 
 	// Newton's method
 	for {
-		if (j == 500) {
-			root = complex(rand_double(2) - 1, rand_double(2) - 1)
+		if j == 500 {
+			root = complex(rand_double(2)-1, rand_double(2)-1)
 			j = 0
 		} else {
 			j++
 		}
 
-		if (i >= 5000) {
+		if i >= 5000 {
 			// nonconv = 1, doesn't converge
 			break
 		}
@@ -77,7 +60,7 @@ func findroots_inner(coefs []complex128, deg int, roots []complex128) []complex1
 		f += p * coefs[deg]
 		root -= f / d
 
-		if (math.Pow(cmplx.Abs(or - root), 2) <= 1e-20) {
+		if math.Pow(cmplx.Abs(or-root), 2) <= 1e-20 {
 			break
 		}
 	}
@@ -95,276 +78,16 @@ func findroots_inner(coefs []complex128, deg int, roots []complex128) []complex1
 	return findroots_inner(coefs, deg-1, roots)
 }
 
-
-var (
-	texture   uint32
-	rotationX float32
-	rotationY float32
-)
-
-const width, height = 800, 600
-
-func init() {
-	// GLFW event handling must run on the main OS thread
-	runtime.LockOSThread()
+func findroots(coefs []complex128, deg int) []complex128 {
+	roots := make([]complex128, 0)
+	roots = findroots_inner(coefs, deg, roots)
+	return roots
 }
 
 func main() {
-	// stuff
-	var ox float64 = 0
-	var oy float64 = 0
-	var xres float64= 1000
-	var yres float64 = 1000
-	var zoom float64 = yres / 5
+	coefs := []complex128{complex(1, 0), complex(0, 0), complex(-1, 0)}
+	roots := findroots(coefs, 2)
 
-	if err := glfw.Init(); err != nil {
-		log.Fatalln("failed to initialize glfw:", err)
-	}
-	defer glfw.Terminate()
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 2)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	window, err := glfw.CreateWindow(width, height, "Cube", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.MakeContextCurrent()
-
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-
-	texture = newTexture("square.png")
-	defer gl.DeleteTextures(1, &texture)
-
-	// setupScene()
-	for !window.ShouldClose() {
-		// drawScene()
-		// gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		// gl.MatrixMode(gl.MODELVIEW)
-		// gl.LoadIdentity()
-		// gl.Translatef(0, 0, -3.0)
-		// gl.Rotatef(rotationX, 1, 0, 0)
-		// gl.Rotatef(rotationY, 0, 1, 0)
-
-		// // rotationX += 0.5
-		// // rotationY += 0.5
-
-		// gl.BindTexture(gl.TEXTURE_2D, texture)
-
-		// gl.Color4f(1, 1, 1, 1)
-
-		// gl.Begin(gl.QUADS)
-
-		// gl.Normal3f(0, 0, 1)
-		// gl.TexCoord2f(0, 0)
-		// gl.Vertex3f(-1, -1, 1)
-		// gl.TexCoord2f(1, 0)
-		// gl.Vertex3f(1, -1, 1)
-		// gl.TexCoord2f(1, 1)
-		// gl.Vertex3f(1, 1, 1)
-		// gl.TexCoord2f(0, 1)
-		// gl.Vertex3f(-1, 1, 1)
-
-		// gl.TexCoord2f(1,1); gl.Vertex2f(1,1);
-		// gl.TexCoord2f(1,0); gl.Vertex2f(1,0);
-		// gl.TexCoord2f(0,0); gl.Vertex2f(0,0);
-		// gl.TexCoord2f(0,1); gl.Vertex2f(0,1);
-		// gl.End()
-
-		gl.MatrixMode(gl.MODELVIEW)
-		gl.PushMatrix()
-		gl.Scaled(zoom, zoom, zoom)
-		gl.Translated((xres/2/zoom)-ox,(yres/2/zoom)-oy,0)
-
-		gl.Enable(gl.BLEND)
-		gl.BlendFunc(gl.ONE, gl.ONE)
-		gl.Disable(gl.DEPTH_TEST)
-		gl.Enable(gl.TEXTURE_2D)
-
-		// pick up here
-		// glBindTexture(GL_TEXTURE_2D,tex)
-		gl.Begin(gl.QUADS)
-
-		// iterate over generated algebraic points
-		var x float32 = 10
-		var y float32 = 10
-		var r float32 = 5
-		gl.Color3f(0, 0, 1)
-		gl.TexCoord2f(1,1); gl.Vertex2f(x+r*16,y+r*16);
-		gl.TexCoord2f(1,0); gl.Vertex2f(x+r*16,y-r*16);
-		gl.TexCoord2f(0,0); gl.Vertex2f(x-r*16,y-r*16);
-		gl.TexCoord2f(0,1); gl.Vertex2f(x-r*16,y+r*16);
-
-		gl.End()
-
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
-}
-
-func newTexture(file string) uint32 {
-	imgFile, err := os.Open(file)
-	if err != nil {
-		log.Fatalf("texture %q not found on disk: %v\n", file, err)
-	}
-	img, _, err := image.Decode(imgFile)
-	if err != nil {
-		panic(err)
-	}
-
-	rgba := image.NewRGBA(img.Bounds())
-	if rgba.Stride != rgba.Rect.Size().X*4 {
-		panic("unsupported stride")
-	}
-	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
-
-	var texture uint32
-	gl.Enable(gl.TEXTURE_2D)
-	gl.GenTextures(1, &texture)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexImage2D(
-		gl.TEXTURE_2D,
-		0,
-		gl.RGBA,
-		int32(rgba.Rect.Size().X),
-		int32(rgba.Rect.Size().Y),
-		0,
-		gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		gl.Ptr(rgba.Pix))
-
-	return texture
-}
-
-func setupScene() {
-	gl.Enable(gl.DEPTH_TEST)
-	gl.Enable(gl.LIGHTING)
-
-	gl.ClearColor(0.5, 0.5, 0.5, 0.0)
-	gl.ClearDepth(1)
-	gl.DepthFunc(gl.LEQUAL)
-
-	ambient := []float32{0.5, 0.5, 0.5, 1}
-	diffuse := []float32{1, 1, 1, 1}
-	lightPosition := []float32{-5, 5, 10, 0}
-	gl.Lightfv(gl.LIGHT0, gl.AMBIENT, &ambient[0])
-	gl.Lightfv(gl.LIGHT0, gl.DIFFUSE, &diffuse[0])
-	gl.Lightfv(gl.LIGHT0, gl.POSITION, &lightPosition[0])
-	gl.Enable(gl.LIGHT0)
-
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	f := float64(width)/height - 1
-	gl.Frustum(-1-f, 1+f, -1, 1, 1.0, 10.0)
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-}
-
-func drawScene() {
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-	gl.Translatef(0, 0, -3.0)
-	gl.Rotatef(rotationX, 1, 0, 0)
-	gl.Rotatef(rotationY, 0, 1, 0)
-
-	rotationX += 0.5
-	rotationY += 0.5
-
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-
-	gl.Color4f(1, 1, 1, 1)
-
-	gl.Begin(gl.QUADS)
-
-	gl.Normal3f(0, 0, 1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, -1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, -1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, 1)
-
-	gl.Normal3f(0, 0, -1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, 1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, 1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, -1)
-
-	gl.Normal3f(0, 1, 0)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, 1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, -1)
-
-	gl.Normal3f(0, -1, 0)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, -1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, 1)
-
-	gl.Normal3f(1, 0, 0)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, -1, -1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, 1)
-
-	gl.Normal3f(-1, 0, 0)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, 1, 1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, -1)
-
-	gl.End()
-}
-
-// Set the working directory to the root of Go package, so that its assets can be accessed.
-func init() {
-	dir, err := importPathToDir("github.com/go-gl/example/gl21-cube")
-	if err != nil {
-		log.Fatalln("Unable to find Go package in your GOPATH, it's needed to load assets:", err)
-	}
-	err = os.Chdir(dir)
-	if err != nil {
-		log.Panicln("os.Chdir:", err)
-	}
-}
-
-// importPathToDir resolves the absolute path from importPath.
-// There doesn't need to be a valid Go package inside that import path,
-// but the directory must exist.
-func importPathToDir(importPath string) (string, error) {
-	p, err := build.Import(importPath, "", build.FindOnly)
-	if err != nil {
-		return "", err
-	}
-	return p.Dir, nil
+	fmt.Print(roots)
+	return
 }
